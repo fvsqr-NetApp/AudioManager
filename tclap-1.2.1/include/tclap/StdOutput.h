30ba213e26fd664c3362e318329d8b364f2ef617
fvsqr <48791253+fvsqr@users.noreply.github.com>
2021-02-24 11:16:48 +0100

    Update StdOutput.h

--- tclap-1.2.1/include/tclap/StdOutput.h
+++ tclap-1.2.1/include/tclap/StdOutput.h
@@ -110,7 +110,7 @@ inline void StdOutput::version(CmdLineInterface& _cmd)
 	std::string progName = _cmd.getProgramName();
 	std::string xversion = _cmd.getVersion();
 
-	std::cout << std::endl << progName << "  versIon: " 
+	std::cout << std::endl << progName << "  verSion: " 
 			  << xversion << std::endl << std::endl;
 }
 
