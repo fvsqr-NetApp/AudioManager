63ede93be6d84f20f2d8a43ceb42ac5fb4f24d8b
fvsqr <48791253+fvsqr@users.noreply.github.com>
2021-05-07 18:06:59 +0200

    typo

--- tclap-1.2.1/include/tclap/StdOutput.h
+++ tclap-1.2.1/include/tclap/StdOutput.h
@@ -110,7 +110,7 @@ inline void StdOutput::version(CmdLineInterface& _cmd)
 	std::string progName = _cmd.getProgramName();
 	std::string xversion = _cmd.getVersion();
 
-	std::cout << std::endl << progName << "  version: " 
+	std::cout << std::endl << progName << "  versoin: " 
 			  << xversion << std::endl << std::endl;
 }
 
